5eabfbbad0906f736ac48ae147d2ed3467112c24
Rene Lopez <rene.cano@hotmail.com>
2016-04-19 23:03:35 -0500

    Added Reducer for Feedbacks

---
+++ src/reducers/feedbacks/feedbacksReducer.js
@@ -0,0 +1,38 @@
+/* Define your initial state here.
+ *
+ * If you change the type from object to something else, do not forget to update
+ * src/container/App.js accordingly.
+ */
+const initialState = [
+  {
+    name:'Orly Chan',
+    position:200
+  },
+  {
+    title:'Satan',
+    price:150
+  },
+  {
+    title:'Harry Potter',
+    price:200
+  }
+];
+
+module.exports = function(state = initialState, action) {
+  /* Keep the reducer clean - do not mutate the original state. */
+  let nextState = Object.assign([], state);
+
+  switch(action.type) {
+
+    case 'BOOKS':
+    {
+      // Modify next state depending on the action and return it
+      return nextState;
+    }
+
+    default: {
+      /* Return original state if no actions were consumed. */
+      return nextState;
+    }
+  }
+}
