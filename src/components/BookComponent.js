3cd9c4a0aa628bbaeddf220ddb766a439a94c168
rene.lopez <rene.lopez.cano@gmail.com>
2016-05-02 11:41:30 -0500

    Added BookComponent for Organization

---
+++ src/components/BookComponent.js
@@ -0,0 +1,19 @@
+/* CAUTION: When using the generators, this file is modified in some places.
+ *          This is done via AST traversal - Some of your formatting may be lost
+ *          in the process - no functionality should be broken though.
+ *          This modifications only run once when the generator is invoked - if
+ *          you edit them, they are not updated again.
+ */
+import React from 'react';
+import BooksContainer from '../containers/BooksContainer';
+import BookDetailContainer from '../containers/BookDetailContainer';
+
+
+let App = () => (
+  <div>
+    <BooksContainer />
+    <BookDetailContainer />
+  </div>
+);
+
+export default App;
