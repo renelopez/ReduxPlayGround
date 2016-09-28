0adbdc0293a68eaaee295ff31991ea693feeff77
rene.lopez <rene.lopez.cano@gmail.com>
2016-09-28 14:53:40 -0500

    Added tests

--- test/components/MainTest.js
+++ test/components/MainTest.js
@@ -10,14 +10,10 @@
 //
 // import Main from 'components/Main';
 //
-// describe('MainComponent', () => {
-//   let MainComponent;
-//
-//   beforeEach(() => {
-//     MainComponent = createComponent(Main);
-//   });
-//
-//   it('should have its component name as default className', () => {
-//     expect(MainComponent.props.className).to.equal('index');
-//   });
-// });
+describe('Test Caliz', () => {
+
+
+  it('One to equal one', () => {
+    expect(1).to.equal(1);
+  });
+});
